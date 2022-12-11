b847a95800a8e3788b5317b865d562d9fbcdd75a
daochang <2051525775@qq.com>
2022-12-12 01:36:03 +0800

    优化

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ module.exports.add = function (data) {
         })
         let result = response.concat(temp)
         dbApi.write(JSON.stringify(result)).catch(err => console.log(err));
-    }).catch(() => {
+    }).catch((err) => {
+        console.log('here');
+        console.log(err);
         dbApi.write('[]').catch((err) => {
             console.log(err);
         })
@@ -28,25 +30,38 @@ function showList(data) {
                 message: '你要操作哪一项?',
                 name: 'uuu',
                 choices: [
+                    {'name': "添加任务", 'value': '-2'},
                     ...data.map((v, k) => {
-                    let taskName = v.isDone?`√ ${v.taskName}`:`_ ${v.taskName}`
-                    return {'name': taskName, 'value': k}
-                }),{'name': "退出", 'value': '-1'}
+                        let taskName = v.isDone ? `√ ${v.taskName}` : `_ ${v.taskName}`
+                        return {'name': taskName, 'value': k}
+                    }), {'name': "退出", 'value': '-1'}
                 ]
             }
         ])
 }
-function askChangeName() {
+
+function askChangeName(name) {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'changeTaskName',
-            message: "新的任务名字:",
+            message: `新的任务名字(\x1b[31m${name}\x1b[0m):`,
         }
     ])
 }
-function showListAction(temp,id) {
-    let action = {'更改任务名称':'2','已完成': '1', '未完成': '0', '删除此任务': '3', '退出': '-1'}
+
+function askNameAdd() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addTaskName',
+            message: "新的任务名字:"
+        }
+    ])
+}
+
+function showListAction(temp, id) {
+    let action = {'更改任务名称': '2', '已完成': '1', '未完成': '0', '删除此任务': '3', '退出': '-1', '添加任务': '-2'}
     inquirer
         .prompt([
             {
@@ -57,7 +72,7 @@ function showListAction(temp,id) {
             }
         ]).then(async ({actionName}) => {
         if (action[actionName] === '2') {
-            let Name = await askChangeName()
+            let Name = await askChangeName(temp[id['uuu']].taskName)
             temp[id['uuu']].taskName = Name['changeTaskName']
         } else if (action[actionName] === '1') {
             temp[id['uuu']].isDone = true
@@ -69,13 +84,21 @@ function showListAction(temp,id) {
         void await dbApi.write(JSON.stringify(temp))
     })
 }
+
 module.exports.showAll = async function () {
     let temp = await dbApi.get().catch(async () => {
         await dbApi.write('[]')
     })
-    if(!temp){
+    if (!temp) {
         temp = await dbApi.get()
     }
     let id = await showList(temp);
-    id['uuu'] !== '-1' ? showListAction(temp, id) : ''
+    if (id['uuu'] === '-2') {
+        let {addTaskName} = await askNameAdd()
+        this.add([addTaskName])
+        return
+    }else if (id['uuu'] === '-1'){
+        return
+    }
+    showListAction(temp, id)
 }
